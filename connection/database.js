d08de51c85e3aa5afc1f51274b78e15530b6f793
lauravivan <lauvivang@gmail.com>
2024-02-13 16:19:53 -0300

    Update env variables

--- connection/database.js
+++ connection/database.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fmeitb9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
